a1486f3ffe911cfdc46f55acdb3e1863d9355733
Avaer Kazmer <a@modules.io>
2022-09-05 06:46:55 -0400

    Bugfix water height

--- cache.h
+++ cache.h
@@ -39,7 +39,7 @@ public:
     float waterFactor;
 
     float getHeight() const {
-      return 0;
+      return WORLD_BASE_HEIGHT;
     }
     bool acceptIndex() const {
       return waterFactor > 0;
